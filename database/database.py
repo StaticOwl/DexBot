229bada7bf69a9fc9a2527917dd70504313a4dd7
StaticOwl <staticowl@nomad>
2023-10-07 22:26:04 -0400

    updating database code and adding more files to make sense

--- database/database.py
+++ database/database.py
@@ -34,13 +34,73 @@ def find_parent(pid):
     except Exception as e:
         print("find_parent", e)
         return False
+    
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        print("find_existing_score", e)
+        return False
+    
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data == '[removed]':
+        return False
+    else:
+        return True
+    
+def transaction_bldr(sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        c.execute("BEGIN TRANSACTION")
+        for s in sql_transaction:
+            try:
+                c.execute(s)
+            except:
+                pass
+        conn.commit()
+        sql_transaction = []
+    
+def sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc, score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ?
+                WHERE parent_id = ?""".format(parent_id, comment_id, parent_data, body, subreddit, int(created_utc), score, parent_id)
+        transaction_bldr(sql)
+    except Exception as e:
+        print("sql_insert_replace_comment", e)
+
+def sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score)
+                VALUES ("{}", "{}", "{}", "{}", "{}", "{}", "{}")""".format(parent_id, comment_id, parent_data, body, subreddit, int(created_utc), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print("sql_insert_has_parent", e)
+
+def sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score)
+                VALUES ("{}", "{}", "{}", "{}", "{}", "{}")""".format(parent_id, comment_id, body, subreddit, int(created_utc), score)
+        transaction_bldr(sql)
+    except Exception as e:
+        print("sql_insert_no_parent", e)
 
 if __name__=="__main__":
     create_table()
     row_counter = 0
     paired_rows = 0
     
-    with open("RC_2015-01", buffer=1000) as f:
+    with open("database/RC_2015-01", buffering=1000) as f:
         for row in f:
             row_counter += 1
             row = json.loads(row)
@@ -51,3 +111,17 @@ if __name__=="__main__":
             subreddit = row['subreddit']
             comment_id = row['id']
             parent_data = find_parent(parent_id)
+
+            #search for comments with a score of 2 or more
+            if score >= 2:
+                if acceptable(body):
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+                    else:
+                        if parent_data:
+                            sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+                            paired_rows += 1
+                        else:
+                            sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
